e1e48470a2ea669541b64b8bbb7ecb19eb4ed420
MineRac <75296368+MineRac@users.noreply.github.com>
2026-06-07 19:34:24 +0400

    1

--- frontend/src/api/inventory.ts
+++ frontend/src/api/inventory.ts
@@ -1,5 +1,9 @@
 export async function getInventory() {
-  const res = await fetch("https://lab2-gamma-lovat.vercel.app/api/inventory");
-  if (!res.ok) throw new Error("Failed to load inventory");
+  const res = await fetch("https://https://github.com/MineRac/lab2/tree/main/backend/api/inventory");
+
+  if (!res.ok) {
+    throw new Error("Failed to load inventory");
+  }
+
   return res.json();
 }
