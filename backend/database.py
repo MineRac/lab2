af77de57b671cad8e627d830b801a2ab1f09f9b1
MineRac <75296368+MineRac@users.noreply.github.com>
2026-06-07 19:05:24 +0400

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,17 +1,21 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///warehouse.db"
+DATABASE_URL = "sqlite:///./inventory.db"
 
 engine = create_engine(
     DATABASE_URL,
-    connect_args={
-        "check_same_thread": False
-    }
+    connect_args={"check_same_thread": False}
 )
 
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
-)
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+
+Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
