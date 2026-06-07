7a10d9c3b47ba3272b1a8d06c45d2314cb3f1a6d
MineRac <75296368+MineRac@users.noreply.github.com>
2026-06-07 19:38:18 +0400

    1

--- frontend/src/api/inventory.ts
+++
@@ -1,9 +0,0 @@
-export async function getInventory() {
-  const res = await fetch("https://https://github.com/MineRac/lab2/tree/main/backend/api/inventory");
-
-  if (!res.ok) {
-    throw new Error("Failed to load inventory");
-  }
-
-  return res.json();
-}
