59ba9291ffa4f869877dc7ed2650b38f34480098
MineRac <75296368+MineRac@users.noreply.github.com>
2026-06-07 19:06:01 +0400

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,81 +1,12 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+from database import Base, engine
 
-from pydantic import BaseModel
-
-import numpy as np
-
-from stable_baselines3 import SAC
-from stable_baselines3.common.vec_env import (
-    DummyVecEnv,
-    VecNormalize
-)
-
-from inventory_env import StableInventoryEnv
-
+from api.inventory import router as inventory_router
 
 app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# создать таблицы
+Base.metadata.create_all(bind=engine)
 
-
-env = DummyVecEnv([
-    lambda: StableInventoryEnv()
-])
-
-env = VecNormalize.load(
-    "vec_normalize.pkl",
-    env
-)
-
-env.training = False
-env.norm_reward = False
-
-model = SAC.load(
-    "sac_inventory"
-)
-
-
-class PredictionRequest(BaseModel):
-    inventory: int
-    backlog: int = 0
-
-
-@app.get("/")
-def root():
-
-    return {
-        "status": "ok"
-    }
-
-
-@app.post("/predict")
-def predict(data: PredictionRequest):
-
-    obs = np.array(
-        [
-            [
-                data.inventory,
-                data.backlog
-            ]
-        ],
-        dtype=np.float32
-    )
-
-    obs = env.normalize_obs(obs)
-
-    action, _ = model.predict(
-        obs,
-        deterministic=True
-    )
-
-    return {
-        "inventory": data.inventory,
-        "recommended_order": int(action[0])
-    }
+# подключаем роуты
+app.include_router(inventory_router, prefix="/api")
