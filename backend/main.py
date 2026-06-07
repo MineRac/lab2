9e9d2f8123727833d553314fdc821e7211ddbb24
MineRac <75296368+MineRac@users.noreply.github.com>
2026-06-07 20:01:42 +0400

    1

--- backend/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from database import Base, engine
-from inventory import router as inventory_router
-
-Base.metadata.create_all(bind=engine)
-
-app = FastAPI()
-
-app.include_router(inventory_router, prefix="/api")
