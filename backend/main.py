f56ebe0af76631856a9591c16eff6da29fafb706
MineRac <75296368+MineRac@users.noreply.github.com>
2026-06-07 18:38:37 +0400

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,24 +1,14 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+import numpy as np
+
 from stable_baselines3 import SAC
-from stable_baselines3.common.vec_env import DummyVecEnv
-from stable_baselines3.common.vec_env import VecNormalize
+from stable_baselines3.common.vec_env import (
+    DummyVecEnv,
+    VecNormalize
+)
 
 from inventory_env import StableInventoryEnv
 
 app = FastAPI()
-
-env = DummyVecEnv([
-    lambda: StableInventoryEnv()
-])
-
-env = VecNormalize.load(
-    "vec_normalize.pkl",
-    env
-)
-
-env.training = False
-env.norm_reward = False
-
-model = SAC.load(
-    "sac_inventory"
-)
