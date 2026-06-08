37cdab0786283a1063c5bec3676428f663e116d0
MineRac <75296368+MineRac@users.noreply.github.com>
2026-06-08 14:52:58 +0400

    Delete api/predictions directory

--- api/predictions/model-info.ts
+++
@@ -1,5 +0,0 @@
-import { withAuth } from '../../lib/authMiddleware';
-
-export default withAuth(async (req, res) => {
-  res.json({ accuracy: 94.3, predictionsCount: 2847, avgConfidence: 91.8, savings: 1200000 });
-});
